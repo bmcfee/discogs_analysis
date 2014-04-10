4c6b76f1abf9a24b69d8834ad8e040fd03112178
Brian McFee <brm2132@columbia.edu>
2014-04-10 10:11:20 -0400

    adding gordon extractor

---
+++ code/gordon_get_track_info.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+import ujson as json
+
+import gordon
+
+def get_track_info(collection_name=None, output_file=None):
+
+    collection = gordon.Collection.query.filter_by(name=collection_name).limit(1).all()
+
+    if len(collection) == 0:
+        raise ValueError('Empty collection: %s' % collection_name)
+
+    track_data = []
+    for t in collection.tracks:
+        track_data.append({'title': unicode(t.title), 'artist': unicode(t.artist), 'duration': int(t.secs)})
+
+    with open(output_file, 'w') as f:
+        json.dump(track_data, f)
+
+    pass
+
+
+def process_arguments(args):
+
+    parser = argparse.ArgumentParser(description='Get a collection out of gordon')
+
+    parser.add_argument('collection_name', type=str, action='store', help='Which collection to extract')
+    parser.add_argument('output_file', type=str, action='store', help='Path to store json output')
+
+    return vars(parser.parse_args(args))
+
+if __name__ == '__main__':
+    args = process_arguments(sys.argv[1:])
+
+    get_track_info(**args)
